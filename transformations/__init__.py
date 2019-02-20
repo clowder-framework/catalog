3899e0104e6b3aa38e57b8be4cc2cf0346090055
Bing Zhang <bing@illinois.edu>
2019-02-20 16:11:03 -0600

    add auth.py to auth the login

--- transformations/__init__.py
+++ transformations/__init__.py
@@ -1,10 +1,13 @@
 import os
 
 from flask import Flask, render_template
+from . import auth
+
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    app.register_blueprint(auth.bp)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'transformations.mongo'),
