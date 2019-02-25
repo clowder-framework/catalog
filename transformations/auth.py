0e4a5cc655bf8fbcebe71152d2a5464f17797d75
Bing Zhang <bing@illinois.edu>
2019-02-25 16:22:06 -0600

    add Ldap search in groups

--- transformations/auth.py
+++ transformations/auth.py
@@ -15,16 +15,31 @@ def login():
         password = request.form['password']
         error = None
         ldap_hostname = current_app.config['LDAP_HOSTNAME']
-        ldap_login = ldap.initialize(ldap_hostname)
+        ldap_client = ldap.initialize(ldap_hostname)
+        ldap_client.set_option(ldap.OPT_REFERRALS, 0)
         user_dn = "uid=%s,%s,%s" % (username, current_app.config['LDAP_USER_DN'], current_app.config['LDAP_BASE_DN'])
-        base_dn = "%s,%s" % (current_app.config['LDAP_GROUP_DN'], current_app.config['LDAP_BASE_DN'])
+        group_dn = "%s,%s" % (current_app.config['LDAP_GROUP_DN'], current_app.config['LDAP_BASE_DN'])
         try:
-            ldap_login.protocol_version = ldap.VERSION3
-            ldap_login.simple_bind_s(user_dn, password)
+            ldap_client.protocol_version = ldap.VERSION3
+            ldap_client.simple_bind_s(user_dn, password)
         except Exception as ex:
             error = ex
 
-        ldap_login.unbind_s()
+        try:
+
+            search_scope = ldap.SCOPE_SUBTREE
+            search_filter = "(&(objectClass=%s)(memberOf=cn=%s,%s)(uid=%s))" % (current_app.config['LDAP_OBJECTCLASS'],
+                                                                                current_app.config['LDAP_GROUP'],
+                                                                                group_dn, username)
+            print(search_filter)
+            ldap_result = ldap_client.search_s(current_app.config['LDAP_BASE_DN'], search_scope, search_filter)
+            if 0 == len(ldap_result):
+                error = "cannot find in this group"
+
+        except Exception as ex:
+            error = ex
+
+        ldap_client.unbind_s()
         user = dict()
         user['id'] = username
         if error is None:
