b0ca38595f052c0e825979f1e7f2a5e8a0af3621
Bing Zhang <bing@illinois.edu>
2019-02-27 16:58:32 -0600

    read submitted from db and show them on table

--- transformations/publish.py
+++ transformations/publish.py
@@ -17,22 +17,25 @@ def approve():
         # pass json to the template
         db = get_db()
         collection = db[current_app.config['TRANSFORMATIONS_DATABASE_NAME']]
-        transformations = collection.transformations.find({"submitted": True}, {'_id': 0})
+        transformations = collection.transformations.find({"submitted": True})
         transformations_data = []
         try:
             for data_tuple in transformations:
-                transformations_data.append(data_tuple)
+                data = dict()
+                data['status'] = data_tuple.get('status')
+                data['transformation_type'] = data_tuple.get('transformation_type')
+                data['name'] = data_tuple.get('transformation_id')
+                transformations_data.append(data)
         except pymongo.errors.ServerSelectionTimeoutError as err:
             print(err)
 
-        # transformations_data = [{"name": "extractor1", "status": 4}, {"name": "extractor2", "status": 5}]
         return render_template('publish/approve.html', columns=transformations_data)
     elif request.method == 'POST':
         form_dic = request.form.to_dict()
 
-        columns = [{"name": "extractor1", "status": 4}, {"name": "extractor2", "status": 5}]
+        transformations = [{"name": "extractor1", "status": 4}, {"name": "extractor2", "status": 5}]
 
         transformation_id = form_dic.get('transformationId')
         print(transformation_id)
-        return render_template('publish/approve.html', columns=columns)
+        return render_template('publish/approve.html', columns=transformations)
 
