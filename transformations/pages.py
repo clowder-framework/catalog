27c81f703be930e8b9eddf412ece211ada969d5e
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2019-02-27 14:29:48 -0600

    Removed hardcoded database name.

--- transformations/pages.py
+++ transformations/pages.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, request, g, redirect, url_for
+    Blueprint, render_template, request, g, redirect, url_for, current_app
 )
 from transformations.db import get_db
 
@@ -9,7 +9,7 @@ bp = Blueprint('pages', __name__)
 @bp.route('/')
 def home():
     db = get_db()
-    collection = db["toolscatalog"]
+    collection = db[current_app.config['TRANSFORMATIONS_DATABASE_NAME']]
     transformations = collection.transformations.find()
 
     return render_template('pages/home.html', transformations = transformations)
