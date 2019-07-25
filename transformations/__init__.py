8c2edd435d12044d70adbe0a9aaff311c5302fc4
Mark Fredricksen <mfrdrcks@illinois.edu>
2019-07-25 11:04:20 -0500

    Updated any static pages in the templates to use url_for, allowing a variable place for the static pages. Put a URL_PREFIX in config.py, allowing for prefixing to the URL in case of proxy_pass forwarding. Modified the order of __init__.py and added a line to handle the URL_PREFIX.

--- transformations/__init__.py
+++ transformations/__init__.py
@@ -6,12 +6,21 @@ from . import auth
 from . import publish
 from . import pages
 from . import api
+from werkzeug import DispatcherMiddleware
 from transformations.db import get_db
 
 
 def create_app(test_config=None):
-    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {app.config['URL_PREFIX']: app.wsgi_app})
+    # create and configure the app
     app.register_blueprint(auth.bp)
     app.register_blueprint(publish.bp)
     app.register_blueprint(pages.bp)
@@ -19,13 +28,6 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev'
     )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
