c1351fb554bf6f9ad4b352d87dee46938c8a02b8
Mark Fredricksen <mfrdrcks@illinois.edu>
2020-06-22 18:22:05 -0500

    Added the index creation to get_db, but only where the db is not already populated. This should create the index upon start of the app, and in local testing has been effective even on an empty database.

--- transformations/db.py
+++ transformations/db.py
@@ -5,6 +5,9 @@ def get_db():
     if 'db' not in g:
         client = MongoClient(current_app.config['TRANSFORMATIONS_DATABASE_URI'])
         g.db = client
+        database = client.get_database(current_app.config['TRANSFORMATIONS_DATABASE_NAME'])
+        collection = database.get_collection("transformations")
+        collection.create_index([('transformationId', TEXT), ('description', TEXT)])
     return g.db
 
 def close_db(e=None):
